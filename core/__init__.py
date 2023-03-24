ac5425784467025a6f53156aa6854885547209bb
Josh Shepherd <joshuashepherd877@gmail.com>
2023-03-24 22:44:28 +0000

    add migrations

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from werkzeug.security import generate_password_hash
+from flask_migrate import Migrate
 
 # filesystem imports
 from dotenv import load_dotenv, find_dotenv
@@ -30,6 +31,8 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     db.init_app(app)
+
+    migrate = Migrate(app, db)
 
     @app.route('/test')
     def test():
