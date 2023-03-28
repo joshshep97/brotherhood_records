337bf78ca9ca2080f23bfd222f506932cf62113a
Josh Shepherd <joshuashepherd877@gmail.com>
2023-03-28 17:48:20 +0100

    add build deps

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from core import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run()
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,4 @@
 def test_index(client):
     response = client.get('/products/')
     assert b'<h1>Brotherhood Records</h1>' in response.data
+    assert response.status_code == 200
