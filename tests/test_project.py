98bde11f7e6c71c3677c4b212d060d7f2ce46819
Josh Shepherd <joshuashepherd877@gmail.com>
2023-03-31 11:31:57 +0100

    add testing

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,6 +2,7 @@ def test_index(client):
     response = client.get('/products/')
     assert b'<h1>Brotherhood Records</h1>' in response.data
     assert response.status_code == 200
+    assert b'Login' in response.data
 
 def test_login(client):
     response = client.get('/auth/login/', data={'username': 'test', 'password': 'test'})
